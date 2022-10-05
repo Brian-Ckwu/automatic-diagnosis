8f2b641d2a21539085e970a0d12ba5a10df52994
Brian-Ckwu <sonny40920710@gmail.com>
2022-10-05 10:43:15 +0800

    add load_yaml()

--- DQN_liu_ACL_2018/utils.py
+++ DQN_liu_ACL_2018/utils.py
@@ -1,3 +1,4 @@
+import yaml
 import json
 from typing import Any
 from pathlib import Path
@@ -7,3 +8,6 @@ def load_json(file: str):
 
 def save_json(obj: Any, f: str, indent: int = 4):
     return Path(f).write_text(json.dumps(obj, indent=indent))
+
+def load_yaml(path: str) -> dict:
+    return yaml.safe_load(Path(path).read_text())
